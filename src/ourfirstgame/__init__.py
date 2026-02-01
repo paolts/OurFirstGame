d8f513a744a9252cc7e5e4127d835652a37f3328
paolts <leopaoletti09@gmail.com>
2026-02-01 17:19:04 +0100

    primo prototipo progetto con pygame

---
+++ src/ourfirstgame/__init__.py
@@ -0,0 +1,33 @@
+#Leonardo Paoletti
+#primo progetto videogiochi
+
+def main_loop():
+    import pygame
+
+    pygame.init()
+
+    # lo screen (con titolo)
+    screen = pygame.display.set_mode((1320, 650))
+    pygame.display.set_caption("Il mio primo gioco con PyGame!")
+
+    running = True
+
+    while running:
+        #X di chiusura in alto
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_q]:
+            running = False
+        
+        
+        #schermo di verde
+        screen.fill("green")
+
+        #aggiorna il contenuto dello schermo
+        pygame.display.flip()
+
+    #Chiude pygame
+    pygame.quit()
